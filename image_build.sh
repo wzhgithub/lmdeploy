8eb51fc247adc897b38f9a296d046178d21d5659
zh.wang <zh.wang@nio.com>
2024-07-25 11:52:05 +0800

    - u

--- image_build.sh
+++ image_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 branch_name=$(git rev-parse --abbrev-ref HEAD)
+branch_name=${branch_name//\//-}
 commit_id=$(git rev-parse HEAD)
 nohup sudo docker build -t lmdeploy:${branch_name}-${commit_id} -f docker/lmdeploy_local.dockerfile . &
